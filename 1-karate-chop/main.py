43dc443c4107fe51cd1160a9d952c95b0338b907
Mateusz Banaś <m.banas@createit.pl>
2018-09-13 16:26:59 +0200

    Larger ranges tests, corrected old implementations

--- 1-karate-chop/main.py
+++ 1-karate-chop/main.py
@@ -24,22 +24,29 @@ def binary_chop_recursive(needle, haystack, i=0):
 # note: every time i have to look at test data and go step by step
 # error: check if count is zero
 # remembered from previous: modify the main index to keep track on whole index, not active chunk
+# note: had to correct after adding larger ranges to test
 def binary_chop_iteration(needle, haystack):
-    count = len(haystack)
+    count = len(haystack) # 16
 
     if count == 0:
         return -1
 
-    max_iterations = int(count / 2) + 1
     haystack_index = int(count / 2)
+    max_iterations = haystack_index + 1
+    prev_index = count
 
     for i in range(0, max_iterations):
+        _prev_index = haystack_index
+
         if needle == haystack[haystack_index]:
             return haystack_index
         elif needle > haystack[haystack_index]:
             haystack_index += int((count - haystack_index)/2)
         elif needle < haystack[haystack_index]:
-            haystack_index = int(haystack_index/2)
+            haystack_index -= abs(int((prev_index - haystack_index)/2))
+            if _prev_index == haystack_index:
+                haystack_index -= 1
+        prev_index = _prev_index
 
     return -1
 
@@ -47,6 +54,7 @@ def binary_chop_iteration(needle, haystack):
 # errors: endless loop
 # note: at one point failed only my additional test
 # note: I'm not happy that I had to use i and max_iterations
+# note: had to correct after adding larger ranges to test
 def binary_chop_while_iteration(needle, haystack):
     count = len(haystack)
 
@@ -54,29 +62,30 @@ def binary_chop_while_iteration(needle, haystack):
         return -1
 
     start = 0
-    end = int(count/2)
+    end = count-1
 
     max_iterations = int(count / 2) + 1
     i = 0
 
-    while start != end and i <= max_iterations:
-        if needle == haystack[end]:
-            start = end
-        elif needle > haystack[end]:
-            start = end
-            end += int((count-end)/2)
-        elif needle < haystack[end]:
-            end = int((end-start)/2)
+    mid = end
+
+    while needle != haystack[mid] and i <= max_iterations:
+        mid = start + int((end - start) / 2)
+        if needle > haystack[mid]:
+            start = mid
+        elif needle < haystack[mid]:
+            end = mid
+
         i += 1
 
-    if haystack[end] == needle:
-        return end
+    if haystack[mid] == needle:
+        return mid
     return -1
 
 
 # In this approach there is no need to pass 'global' haystack index as it's assigned as a key to every value
 # Also instead of dict it could be a list of tuples (global_index, value)
-# Downside: have to iterate all values to build a dict at the beginning
+# Downside: have to iterate all values to build a dict at the beginning (memory error on very large range (0, 2**60)
 # Errors: endless recursion (had to add `elif middle_point == first_index`)
 def binary_chop_dict_recursive(needle, haystack):
 
@@ -101,13 +110,15 @@ def _chop_dict_recursive(needle, haystack: dict):
     middle_point = first_index + int((last_index-first_index) / 2)
     middle_point_str = str(middle_point)
 
+    chunk_mid = int(count/2)
+
     if needle == haystack[middle_point_str]:
         return middle_point
     elif middle_point == first_index:
         return -1
     elif needle > haystack[middle_point_str]:
-        return _chop_dict_recursive(needle, dict(list(haystack.items())[middle_point+1:]))
+        return _chop_dict_recursive(needle, dict(list(haystack.items())[chunk_mid+1:]))
     elif needle < haystack[middle_point_str]:
-        return _chop_dict_recursive(needle, dict(list(haystack.items())[:middle_point]))
+        return _chop_dict_recursive(needle, dict(list(haystack.items())[:chunk_mid]))
 
 
--- 1-karate-chop/main_test.py
+++ 1-karate-chop/main_test.py
@@ -1,5 +1,10 @@
 import pytest
 from main import *
+from random import randint
+
+large_list_len = 2*40
+print(large_list_len)
+large_list_num = randint(0, large_list_len-1)
 
 test_data = [
   (-1, (3, [])),
@@ -24,6 +29,9 @@ test_data = [
   (-1, (6, [1, 3, 5, 7])),
   (-1, (8, [1, 3, 5, 7])),
   (5, (6, [1, 2, 3, 4, 5, 6, 7])),
+  (10, (10, range(0, 16))),
+  (0, (0, range(0, 16))),
+  (large_list_num, (large_list_num, range(0, large_list_len)))
 ]
 
 
