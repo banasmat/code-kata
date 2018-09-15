befa4fee9246f07aa67d7b9cbabb55c4d37edbfc
Mateusz Banaś <m.banas@createit.pl>
2018-09-15 21:53:08 +0200

    Finding similar numbers in bank-ocr kata

--- 2-bank-ocr/main_2.py
+++ 2-bank-ocr/main_2.py
@@ -55,13 +55,13 @@ num_dict = {
     ): '9',
 }
 
-char_h = 3
-char_w = 3
 
 def decode_number_from_image(num_image: str, validate=True) -> str:
 
     input_w = 27
     chars_len = 9
+    char_h = 3
+    char_w = 3
 
     nums_matrix = [[[' ' for x in range(char_w)] for y in range(char_h)] for z in range(chars_len)]
 
@@ -97,10 +97,6 @@ def decode_number_from_image(num_image: str, validate=True) -> str:
     return result + suffix
 
 
-def find_similar_numbers(number_image, recognized_number=None):
-    pass
-
-
 def is_number_valid(number: str) -> bool:
     checksum = 0
     i = 1
@@ -109,3 +105,42 @@ def is_number_valid(number: str) -> bool:
         i += 1
 
     return checksum % 11 == 0
+
+
+def find_similar_numbers(number_image, recognized_number=None) -> tuple:
+
+    _num_dict = num_dict.copy()
+
+    if recognized_number is not None:
+        del _num_dict[recognized_number]
+
+    results = []
+
+    number_image = list(number_image)
+
+    for row_i, row in enumerate(number_image):
+
+        for char_i, char in enumerate(row):
+            if char == ' ':
+                possible_chars = ('|', '_')
+            else:
+                possible_chars = (' ',)
+            initial_char = char
+            for replacement_char in possible_chars:
+                number_image[row_i] = __assign_tuple_value(number_image[row_i], char_i, replacement_char)
+
+                try:
+                    results.append(num_dict[tuple(number_image)])
+                except KeyError:
+                    continue
+
+            number_image[row_i] = __assign_tuple_value(number_image[row_i], char_i, initial_char)
+
+    return tuple(results)
+
+
+def __assign_tuple_value(tup, val_i, val):
+    tup = list(tup)
+    tup[val_i] = val
+    tup = tuple(tup)
+    return tup
